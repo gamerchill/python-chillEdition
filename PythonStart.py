1dae5572edab43304d65e5f0ba44f9ea46818fcc
Chill <98439157+gamerchill@users.noreply.github.com>
2024-02-14 11:51:55 -0800

    Update and rename PythonStart to PythonStart.py

---
+++ PythonStart.py
@@ -0,0 +1,46 @@
+#This is a comment
+
+"""
+Multi
+line 
+comment
+"""
+#variables
+
+aString = "world"
+number = 5
+decimals = 2.14
+
+#Print statements
+print("Hello World") 
+print(f"String polarization is cool. Glad I'm in this {aString}")
+print(type("you can see what type of object something is, with the type() constructor"))
+print(type(8))
+
+#variable casting is also pretty cool
+something0 = float(8)
+something1 = str(90)
+
+# user input
+
+favoriteColor = str(input("What is for favorite color?\n))
+print(f"{favoriteColor} is my favorite color too!")
+
+#lists are cool too yk
+listExample1 = ["This is a list.", " It requires brackets", "and commas", "although this particular one is a string list"]
+listExample2 = [1, 5, 9, 100] #integer list
+listExample3 = [true, false, true, true] # boolean list
+listExample4 = ["lists can have anything, really", 69, 4.20, True]
+
+print("you can print out certain elements in a list like this:\n", listExample[1])
+            #listExample4 prints out "69"
+#there is also list constructors as well
+
+list1 = list(("Apple", "Banana", "Cherry", 31, False, 3.14)) #Note the double (())
+
+#you can also add and remove stuff from lists
+     
+
+
+            
+            
